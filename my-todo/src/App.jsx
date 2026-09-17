76c86a89e042df913246d049e2b9bafc23beac66
takarea <157473679+takarea@users.noreply.github.com>
2026-09-17 15:30:47 +0900

    add:todo list

--- my-todo/src/App.jsx
+++ my-todo/src/App.jsx
@@ -1,19 +1,32 @@
 import { useState } from "react";
-import heroImg from "./assets/hero.png";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
+// import heroImg from "./assets/hero.png";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "./assets/vite.svg";
 import "./App.css";
 
-function TodoItem({ text}) {
-  return <li>{text}</li>;
-}
+// function TodoItem({ text }) {
+// return <li>{text}</li>;
+// }
 
 export default function App() {
-  const items= ['牛乳を買う', 'レポート提出'];
-  return (
-    <ul>
-      {items.map((todoText,i)=><TodoItem key={i} text={todoText}/>)}
-    </ul>
-  );
+    const [todos, setTodos] = useState([]); // 変化するデータ
+    const [text, setText] = useState("");
+    const add = () => {
+        const trimmedText = text.trim();
+        if (!trimmedText) return;
+        setTodos([...todos, { id: crypto.randomUUID(), text: trimmedText }]);
+        setText("");
+    };
+    return (
+        <div>
+            <input value={text} onChange={(e) => setText(e.target.value)} />
+            <button onClick={add}>追加</button>
+            <ul>
+                {todos.map((todo, i) => (
+                    <li key={todo.id}>{todo.text}</li>
+                ))}
+            </ul>
+            <p>残り {todos.length} 件</p>
+        </div>
+    );
 }
-
